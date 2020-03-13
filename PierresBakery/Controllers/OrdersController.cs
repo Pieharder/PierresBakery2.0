da33d5bfcaa672cdaa3850e4a6430ffe492175e9
Geoff Goetz <geoff.goetz503@gmail.com>
2020-03-13 13:28:51 -0700

    Change file name for accuracy and add first path

---
+++ PierresBakery/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresBakery.Models;
+using System.Collections.Generic;
+
+namespace PierresBakery.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+  }
+}
