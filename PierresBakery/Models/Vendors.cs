7edb06dbe5814e11ebf0b8780d9cf1194f77dcba
Geoff Goetz <geoff.goetz503@gmail.com>
2020-03-13 14:00:39 -0700

    Add relevant methods  and fields

---
+++ PierresBakery/Models/Vendors.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+
+
+    public void AddItem(Order order)
+    {
+      Orders.Add(order);
+    }
+  }
+}
